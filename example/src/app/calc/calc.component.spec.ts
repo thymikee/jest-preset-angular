5d214199f73632b4f48589607293bb7b1f259fab
Michał Pierzchała <thymikee@gmail.com>
2017-04-04 23:41:58 +0200

    Update example

--- example/src/app/calc/calc.component.spec.ts
+++ example/src/app/calc/calc.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CalcComponent } from './calc.component';
+import { CalcComponent } from 'app/calc/calc.component';
 
 describe('CalcComponent', () => {
   let component: CalcComponent;
