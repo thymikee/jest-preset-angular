db274d0b984d652e288a85ef5e207a62def41c5e
markwhitfeld <markwhitfeld@users.noreply.github.com>
2017-06-13 10:51:15 +0200

    Pass transformOptions parameter to preprocessor (#48)
    * Bump ts-jest to 20.0.6 and Jest to 20.0.4

--- preprocessor.js
+++ preprocessor.js
@@ -3,12 +3,12 @@ const TEMPLATE_URL_REGEX = /templateUrl:\s*('|")(\.\/){0,}(.*)('|")/g;
 const STYLE_URLS_REGEX = /styleUrls:\s*\[\s*((?:'|").*\s*(?:'|")).*\s*.*\]/g;
 const ESCAPE_TEMPLATE_REGEX = /(\${|\`)/g;
 
-module.exports.process = (src, path, config) => {
+module.exports.process = (src, path, config, transformOptions) => {
   if (path.endsWith('.html')) {
     src = src.replace(ESCAPE_TEMPLATE_REGEX, '\\$1');
   }
   src = src
     .replace(TEMPLATE_URL_REGEX, 'template: require($1./$3$4)')
     .replace(STYLE_URLS_REGEX, 'styles: []');
-  return process(src, path, config);
+  return process(src, path, config, transformOptions);
 };
