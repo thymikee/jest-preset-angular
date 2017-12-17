927c2f586ac507c6e220113eebea7281659a8087
Ahn Pham <anhpnnd@gmail.com>
2017-12-17 16:50:09 +0100

    Fix app compilation

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {trigger, transition, style, animate} from '@angular/animations';
 
 @Component({
@@ -14,7 +14,7 @@ export class AppComponent {
   variableWithPrecedingDolar = 1234;
 }
 
-function slideToLeft() {
+export function slideToLeft() {
   return trigger('routerTransition', [
     transition(':enter', [
       style({transform: 'translateX(200%)', position: 'fixed', width: '100%'}),
