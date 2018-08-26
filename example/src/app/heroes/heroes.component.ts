14221e6599760d252b6011ce09b07dc1691d5ef8
Parker Mauney <5124113+ParkerM@users.noreply.github.com>
2018-08-26 17:15:01 -0500

    Add heroes component with inline mocked service provider (#110)

---
+++ example/src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../service/hero';
+import { HeroService } from '../service/hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
