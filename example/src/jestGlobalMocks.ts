e3d1638a32336861a487739470c030af20565732
Max Ivanov <omaxphp@yandex.ru>
2018-11-28 15:55:15 +0300

    fix: add mock for BrowserAnimationsModule

--- example/src/jestGlobalMocks.ts
+++ example/src/jestGlobalMocks.ts
@@ -35,3 +35,27 @@ Object.defineProperty(document.body.style, 'transform', {
     };
   },
 });
+
+
+/**
+ * ISSUE: https://github.com/thymikee/jest-preset-angular/issues/170
+ * Workaround: https://github.com/angular/angular/issues/24094
+ */
+if (document.body.style.animation === undefined && CSSStyleDeclaration) {
+  CSSStyleDeclaration.prototype.animation = '';
+}
+
+if (document.body.style['animation-name'] === undefined && CSSStyleDeclaration) {
+  CSSStyleDeclaration.prototype['animation-name'] = '';
+  CSSStyleDeclaration.prototype['animationName'] = '';
+}
+
+if (document.body.style['animation-duration'] === undefined && CSSStyleDeclaration) {
+  CSSStyleDeclaration.prototype['animation-duration'] = '';
+  CSSStyleDeclaration.prototype['animationDuration'] = '';
+}
+
+if (document.body.style['animation-play-state'] === undefined && CSSStyleDeclaration) {
+  CSSStyleDeclaration.prototype['animation-play-state'] = '';
+  CSSStyleDeclaration.prototype['animationPlayState'] = '';
+}
