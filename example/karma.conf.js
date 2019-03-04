977ff00440dd7e87a076d0117cc818e1576f8a86
Michał Pierzchała <thymikee@gmail.com>
2019-03-04 18:52:52 +0100

    bump more deps and rename test.ts -> karmaTest.ts

--- example/karma.conf.js
+++ example/karma.conf.js
@@ -16,10 +16,10 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      { pattern: './src/karmaTest.ts', watched: false }
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      './src/karmaTest.ts': ['@angular/cli']
     },
     mime: {
       'text/x-typescript': ['ts','tsx']
