a8df6c44012ecdb2ef4a15613bde1811e0a50e9c
Ahn <anhpnnd@gmail.com>
2020-10-04 13:13:52 +0200

    refactor(config): use `readConfiguration` from `@angular/compiler-cli`
    Introduce `NgJestConfig` which extends `ts-jest` config class `ConfigSet`. In this class, we override `readTsConfig` to use `readConfiguration` from `@angular-compiler/cli` to retrieve tsconfig options as well as Angular compiler options.

---
+++ src/__tests__/ng-jest-config.spec.ts
@@ -0,0 +1,61 @@
+import { join } from 'path';
+
+import { NgJestConfig } from '../config/ng-jest-config';
+
+describe('NgJestConfig', () => {
+  describe('readTsConfig', () => {
+    test('should return config including Angular compiler config with tsconfig as a string from ts-jest option', () => {
+      const configFilePath = join(__dirname, 'tsconfig-ve.json');
+      const ngJestConfig = new NgJestConfig({
+        globals: {
+          'ts-jest': {
+            tsconfig: configFilePath,
+          },
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any);
+      const config = ngJestConfig.parsedTsConfig;
+
+      delete config.options.basePath;
+      delete config.options.baseUrl;
+      expect(config.options.configFilePath).toEqual(configFilePath);
+      delete config.options.configFilePath;
+      delete config.options.genDir;
+      expect(config.options).toMatchSnapshot();
+    });
+
+    test('should return config including Angular compiler config with tsconfig as an object from ts-jest option', () => {
+      const ngJestConfig = new NgJestConfig({
+        cwd: __dirname,
+        globals: {
+          'ts-jest': {
+            tsconfig: {
+              resolveJsonModule: true,
+            },
+          },
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any);
+      const config = ngJestConfig.parsedTsConfig;
+
+      delete config.options.basePath;
+      delete config.options.baseUrl;
+      expect(config.options.configFilePath).toEqual(join(__dirname, 'tsconfig.json'));
+      delete config.options.configFilePath;
+      delete config.options.genDir;
+      expect(config.options).toMatchSnapshot();
+    });
+
+    test('should return config including Angular compiler config without tsconfig defined in ts-jest option', () => {
+      const ngJestConfig = new NgJestConfig(Object.create(null));
+      const config = ngJestConfig.parsedTsConfig;
+
+      delete config.options.basePath;
+      delete config.options.baseUrl;
+      expect(config.options.configFilePath).toEqual(join(process.cwd(), 'tsconfig.json'));
+      delete config.options.configFilePath;
+      delete config.options.genDir;
+      expect(config.options).toMatchSnapshot();
+    });
+  });
+});
