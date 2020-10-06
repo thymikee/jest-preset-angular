2f21813ff17dfa849a4141239e50f5b80e8d378d
Ahn <anhpnnd@gmail.com>
2020-10-06 20:15:03 +0200

    refactor: introduce `AngularJestTransformer` (#526)
    BREAKING CHANGE:
    Users who are using `import 'jest-preset-angular'` should change to `import 'jest-preset-angular/config/setup-jest'`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export = require('./config/setup-jest');
+import type { TransformedSource, Transformer, TransformOptions } from '@jest/transform';
+import type { Config } from '@jest/types';
+import { TsJestTransformer } from 'ts-jest/dist/ts-jest-transformer';
+
+class AngularJestTransformer extends TsJestTransformer implements Transformer {
+  process(
+    input: string,
+    filePath: Config.Path,
+    jestConfig: Config.ProjectConfig,
+    transformOptions?: TransformOptions,
+  ): TransformedSource | string {
+    return super.process(input, filePath, jestConfig, transformOptions);
+  }
+}
+
+export = new AngularJestTransformer();
