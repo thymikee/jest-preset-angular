d0a3faa3567fddb0e85866f50a32f7e417f61381
Ahn <anhpnnd@gmail.com>
2020-11-03 14:53:51 +0100

    build(test): update e2e tests (#601)
    - Update e2e tests' dependencies
    - Add an example of Angular library
    - Switch `async` to `waitForAsync` since `async` is deprecated in favor of `waitForAsync`
    - Add `webpack` as optional dep, add `@ngtools/webpack` as dev dep

--- e2e/test-app-v10/src/app/app.component.spec.ts
+++ e2e/test-app-v10/src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
