45554878359ad39639fa28475521d342e2c7d2f6
Ahn <anhpnnd@gmail.com>
2020-11-08 16:59:24 +0100

    perf(compiler): cache module resolution with custom compiler host
    Introduce `NgJestCompilerHost` which allows to use module resolution cache which can improve slightly performance.
    
    Besides, with custom TypeScript compiler host, we can also replicate the way `AngularCompilerPlugin` runs `ngcc`

--- src/compiler/ng-jest-compiler.ts
+++ src/compiler/ng-jest-compiler.ts
@@ -1,23 +1,22 @@
 import { CompilerHost, CompilerOptions, createCompilerHost, Diagnostic } from '@angular/compiler-cli';
 import { hasErrors } from '@ngtools/webpack/src/diagnostics';
 import type { Logger } from 'bs-logger';
-import { normalize } from 'path';
+import { DECLARATION_TYPE_EXT } from 'ts-jest/dist/constants';
 import { updateOutput } from 'ts-jest/dist/compiler/instance';
-import { LINE_FEED } from 'ts-jest/dist/constants';
 import type { TTypeScript } from 'ts-jest/dist/types';
 import type * as ts from 'typescript';
 
 import type { NgJestConfig } from '../config/ng-jest-config';
 import { factory as downlevelCtor } from '../transformers/downlevel-ctor';
+import { NgJestCompilerHost } from './compiler-host';
 
 export class NgJestCompiler {
   private _compilerOptions!: CompilerOptions;
   private _program: ts.Program | undefined;
-  private _compilerHost!: CompilerHost;
+  private _compilerHost: CompilerHost | undefined;
+  private _tsHost: NgJestCompilerHost | undefined;
   private _rootNames: string[] = [];
   private _allDiagnostics: Array<ts.Diagnostic | Diagnostic> = [];
-  private readonly _sourceFileCache: Map<string, ts.SourceFile> = new Map<string, ts.SourceFile>();
-  private readonly _memoryHost: Map<string, string> = new Map<string, string>();
   private readonly _logger: Logger;
   private readonly _ts: TTypeScript;
 
@@ -35,21 +34,17 @@ export class NgJestCompiler {
         this._logger.debug({ fileName }, 'getCompiledOutput: update memory host, rootFiles and Program');
 
         this._rootNames = [...this._rootNames, fileName];
-        this._memoryHost.set(fileName, fileContent);
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this._tsHost!.updateMemoryHost(fileName, fileContent);
         this._createOrUpdateProgram();
       }
       this._logger.debug({ fileName }, 'getCompiledOutput: compiling using Program');
 
-      const compiledOutput: string[] = [];
-      const sourceFile = this._program.getSourceFile(fileName);
-      const emitResult = this._program.emit(
-        sourceFile,
-        (_fileName: string, data: string, _writeByteOrderMark: boolean) => {
-          compiledOutput.push(data);
-        },
-        undefined,
-        undefined,
-        {
+      if (fileName.endsWith(DECLARATION_TYPE_EXT)) {
+        return '';
+      } else {
+        const sourceFile = this._program.getSourceFile(fileName);
+        const emitResult = this._program.emit(sourceFile, undefined, undefined, undefined, {
           before: [
             // hoisting from `ts-jest` or other before transformers
             ...(this.ngJestConfig.customTransformers.before as ts.TransformerFactory<ts.SourceFile>[]),
@@ -61,25 +56,27 @@ export class NgJestCompiler {
              */
             downlevelCtor(this._program),
           ],
-        },
-      );
-      if (this.ngJestConfig.shouldReportDiagnostics(fileName)) {
-        this._logger.debug({ fileName }, 'getCompiledOutput: getting diagnostics');
+        });
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const compiledOutput: [string, string] = this._tsHost!.getEmittedResult();
+        if (this.ngJestConfig.shouldReportDiagnostics(fileName)) {
+          this._logger.debug({ fileName }, 'getCompiledOutput: getting diagnostics');
 
-        this._allDiagnostics.push(
-          ...emitResult.diagnostics,
-          ...this._program.getSyntacticDiagnostics(sourceFile),
-          ...this._program.getSemanticDiagnostics(sourceFile),
-        );
-      }
-      if (!hasErrors(this._allDiagnostics)) {
-        this._logger.debug({ fileName }, 'getCompiledOutput: update compiled output including inline source map');
+          this._allDiagnostics.push(
+            ...emitResult.diagnostics,
+            ...this._program.getSyntacticDiagnostics(sourceFile),
+            ...this._program.getSemanticDiagnostics(sourceFile),
+          );
+        }
+        if (!hasErrors(this._allDiagnostics)) {
+          this._logger.debug({ fileName }, 'getCompiledOutput: update compiled output including inline source map');
 
-        return updateOutput(compiledOutput[1], fileName, compiledOutput[0]);
-      } else {
-        this.ngJestConfig.raiseDiagnostics(this._allDiagnostics as ts.Diagnostic[], fileName);
+          return updateOutput(compiledOutput[0], fileName, compiledOutput[1]);
+        } else {
+          this.ngJestConfig.raiseDiagnostics(this._allDiagnostics as ts.Diagnostic[], fileName);
 
-        return '';
+          return '';
+        }
       }
     } else {
       this._logger.debug({ fileName }, 'getCompiledOutput: compiling as isolated module');
@@ -103,7 +100,7 @@ export class NgJestCompiler {
     this._logger.debug({ parsedTsConfig }, '_setupOptions: initializing compiler config');
 
     this._compilerOptions = { ...parsedTsConfig.options };
-    this._rootNames = parsedTsConfig.rootNames.filter((rootName) => !this.ngJestConfig.isTestFile(rootName));
+    this._rootNames = parsedTsConfig.rootNames;
     if (this._compilerOptions.strictMetadataEmit) {
       this._logger.warn(
         `Using Angular compiler option 'strictMetadataEmit' for applications might cause undefined behavior.`,
@@ -115,57 +112,13 @@ export class NgJestCompiler {
         '_setupOptions: creating Compiler Host using @angular/compiler-cli createCompilerHost',
       );
 
-      this._createCompilerHost();
+      this._tsHost = new NgJestCompilerHost(this._logger, this.ngJestConfig, this._compilerOptions);
+      this._compilerHost = createCompilerHost({
+        options: this._compilerOptions,
+        tsHost: this._tsHost,
+      });
       this._createOrUpdateProgram();
     }
-  }
-
-  private _createCompilerHost(): void {
-    this._logger.debug('_createCompilerHost: creating compiler host using @angular/compiler-cli');
-
-    this._compilerHost = createCompilerHost({
-      options: this._compilerOptions,
-    });
-    this._compilerHost.readFile = (fileName: string): string | undefined => {
-      const normalizedFileName = normalize(fileName);
-      let fileContent = this._memoryHost.get(normalizedFileName);
-      if (!fileContent) {
-        fileContent = this._ts.sys.readFile(normalizedFileName, 'utf-8') ?? '';
-        this._memoryHost.set(normalizedFileName, fileContent);
-      }
-
-      return fileContent;
-    };
-    this._compilerHost.getSourceFile = (fileName: string, languageVersion: ts.ScriptTarget) => {
-      const normalizedFileName = normalize(fileName);
-      try {
-        const cached = this._sourceFileCache.get(normalizedFileName);
-        if (cached) {
-          return cached;
-        }
-
-        this._logger.debug(
-          { normalizedFileName },
-          'getSourceFile: cache miss, reading file and create source file to update cache',
-        );
-
-        const content = this._compilerHost.readFile(normalizedFileName);
-        if (content !== undefined) {
-          const sf = this._ts.createSourceFile(normalizedFileName, content, languageVersion, true);
-          this._sourceFileCache.set(fileName, sf);
-
-          return sf;
-        }
-      } catch (e) {
-        this._logger.error(e);
-      }
-
-      return undefined;
-    };
-    this._compilerHost.getCurrentDirectory = () => this.ngJestConfig.cwd;
-    this._compilerHost.getNewLine = () => LINE_FEED;
-    this._compilerHost.getDefaultLibFileName = (options: ts.CompilerOptions) => this._ts.getDefaultLibFilePath(options);
-    this._compilerHost.trace = (message: string) => this._logger.trace(message);
   }
 
   private _createOrUpdateProgram(): void {
