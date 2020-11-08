daf844893c4898a98c030c6f15f74ff81b1d9cb2
Ahn <anhpnnd@gmail.com>
2020-11-08 11:42:07 +0100

    test(e2e): add custom typing e2e test (#612)

---
+++ e2e/test-app-v10-zone-v11/src/app/custom-typings/index.spec.ts
@@ -0,0 +1,13 @@
+function foo() {
+    $.ajax<number>('/foo');
+}
+
+test('foo', () => {
+  expect(2).toBe(2);
+});
+
+test('bar', () => {
+  const terminateReason: TerminatedReason = 'culled';
+
+  expect(terminateReason).toEqual('culled');
+});
