a2166f859b1c89340ee889520595d05fa3cf65dc
Ahn <anhpnnd@gmail.com>
2021-01-08 16:56:20 +0100

    feat(compiler): support ESM for `isolatedModules: false` (#721)
    - Make sure `esModuleInterop` and `allowSyntheticDefaultImports` true to support import CJS into ESM
    - Adjust `module` value correctly according to Jest transform option `supportsStaticESM` and `ts-jest` config `useESM`

---
+++ e2e/test-app-v9/jest-esm-uniso.config.js
@@ -0,0 +1,20 @@
+require('jest-preset-angular/ngcc-jest-processor');
+const baseConfig = require('./jest.config');
+
+/** @type {import('ts-jest/dist/types').ProjectConfigTsJest} */
+module.exports = {
+  ...baseConfig,
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      useESM: true,
+    }
+  },
+  moduleNameMapper: {
+    ...baseConfig.moduleNameMapper,
+    'tslib': '<rootDir>/node_modules/tslib/tslib.es6.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!tslib)'],
+};
