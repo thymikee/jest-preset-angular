8a0bd65b53f68cc28d5073d6d12f6ad935b681cd
Ahn <anhpnnd@gmail.com>
2021-03-04 00:26:11 +0100

    refactor(config): deprecate import setup file from `index.js` (#854)
    DEPRECATION
    Import setup file via `import 'jest-preset-angular'` is deprecated in favor of `import 'jest-preset-angular/setup-jest'`

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const { rootLogger } = require('ts-jest/dist/utils/logger');
+
+rootLogger.warn(
+  "Import setup jest file via `import 'jest-preset-angular';` is deprecated and will be removed in v9.0.0." +
+    " Please switch to `import 'jest-preset-angular/setup-jest';`"
+);
+
 module.exports = require('./build/setup-jest.js');
