9cb1d90c97546255c5c4423f53eef345c314060f
Ahn <anhpnnd@gmail.com>
2021-04-28 17:12:10 +0200

    fix: remove warning message when installing package

--- scripts/paths.js
+++ scripts/paths.js
@@ -6,12 +6,17 @@ const glob = require('glob');
 const rootDir = path.resolve(__dirname, '..');
 const distDir = path.join(rootDir, 'build');
 const examplesRootDir = path.join(rootDir, 'examples');
+const e2eRootDir = path.join(rootDir, 'e2e');
 const projectsToRun = glob
   .sync(`${examplesRootDir}/*`)
   .filter((examplePath) => lstatSync(examplePath).isDirectory() && existsSync(path.join(examplePath, 'package.json')));
+const e2eDirsToRun = glob
+  .sync(`${e2eRootDir}/*`)
+  .filter((e2ePath) => lstatSync(e2ePath).isDirectory() && existsSync(path.join(e2ePath, 'package.json')));
 
 module.exports = {
   rootDir,
   distDir,
   projectsToRun,
+  e2eDirsToRun,
 };
