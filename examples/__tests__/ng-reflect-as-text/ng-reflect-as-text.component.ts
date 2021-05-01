a8cb035af19427062171a52237f48d25d530ac96
Ahn <anhpnnd@gmail.com>
2021-05-01 09:50:16 +0200

    build(devs-infra): convert test apps to example apps (#914)

--- examples/__tests__/ng-reflect-as-text/ng-reflect-as-text.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ng-reflect-as-text',
-  templateUrl: './ng-reflect-as-text.component.html',
-})
-export class NgReflectAsTextComponent {}
--- examples/test-app-v9/src/typings.d.ts
+++
@@ -1,6 +0,0 @@
-/* SystemJS module definition */
-declare let module: NodeModule;
-interface NodeModule {
-  id: string;
-}
-declare module '*.png';
