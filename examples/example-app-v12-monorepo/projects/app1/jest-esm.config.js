fe0e06c0507ec2c4a6a576412563ac5a8cff72fa
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-02 16:58:35 +0900

    test: add v12 monorepo example app

---
+++ examples/example-app-v12-monorepo/projects/app1/jest-esm.config.js
@@ -0,0 +1,13 @@
+require('jest-preset-angular/ngcc-jest-processor');
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular/presets/defaults-esm',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+};
---
+++ examples/example-app-v12-monorepo/projects/app1/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'app1'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('app1');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('app1 app is running!');
+  });
+});
