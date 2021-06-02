37ca7758dc45ae2a84f73ef131f87b25a3300cdc
Suguru Inatomi <suguru.inatomi@gmail.com>
2021-06-02 17:42:37 +0900

    refactor: lint:fix

--- examples/example-app-v12-monorepo/projects/app2/src/app/app.component.spec.ts
+++ examples/example-app-v12-monorepo/projects/app2/src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
