1187893d7a16bd87f96baf6c2e8c088f999bc3c1
Ahn <anhpnnd@gmail.com>
2021-06-05 13:09:43 +0200

    fix: revert `exports` in `package.json`
    See https://github.com/thymikee/jest-preset-angular/issues/941#issuecomment-854580348

---
+++ examples/example-app-v12-monorepo/jest-esm.config.js
@@ -0,0 +1,4 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  projects: ['<rootDir>/projects/app1/jest-esm.config.js', '<rootDir>/projects/app2/jest-esm.config.js'],
+};
