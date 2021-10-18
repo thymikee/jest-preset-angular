d9c03334c3e73af5294ac2c271adca1061ce1c4a
Ahn <anhpnnd@gmail.com>
2021-10-18 16:48:34 +0200

    refactor(transformers): use Angular `downlevel-ctor` transformer
    Since we don't support Angular 9 anymore, it is safe to import `downlevel-ctor` transformer from `@angular/compiler-cli`

--- examples/example-app-v11/src/app/app.component.ts
+++ examples/example-app-v11/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FooService } from './foo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'example-app-v11';
+
+  constructor(private fooService: FooService) {}
+
+  ngOnInit(): void {
+    this.fooService.getFoo();
+  }
 }
