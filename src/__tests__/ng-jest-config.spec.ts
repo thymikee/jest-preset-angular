3a0bfe847e3e5e5f92a71cbcf69c44d9b65b691b
Ahn <anhpnnd@gmail.com>
2021-10-26 10:07:14 +0200

    fix(config): set tsconfig `target` to ES2015 when `target` > ES2016 (#1118)
    Closes #1058

--- src/__tests__/ng-jest-config.spec.ts
+++ src/__tests__/ng-jest-config.spec.ts
@@ -1,3 +1,5 @@
+import ts from 'typescript';
+
 import { NgJestConfig } from '../config/ng-jest-config';
 
 describe('NgJestConfig', () => {
@@ -11,5 +13,21 @@ describe('NgJestConfig', () => {
     expect(compilerOptions.annotationsAs).toBe('decorators');
     expect(compilerOptions.enableResourceInlining).toBe(false);
     expect(compilerOptions.allowJs).toBe(true);
+    expect(compilerOptions.target).toBe(ts.ScriptTarget.ES2015);
+  });
+
+  test('should set typescript target to ES2015 if user is using target higher than ES2016', () => {
+    expect(
+      new NgJestConfig({
+        globals: {
+          'ts-jest': {
+            tsconfig: {
+              target: 'ES2017',
+            },
+          },
+        },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any).parsedTsConfig.options.target,
+    ).toBe(ts.ScriptTarget.ES2015);
   });
 });
