a475e9fe2549f2fb8b93ceb2745bf7792c6592be
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2021-11-17 11:55:56 +0100

    test(e2e): add full Ivy lib test

--- e2e/ng-lib-import/node_modules/my-lib/lib/my-lib.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class MyLibComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-    static ɵfac: i0.ɵɵFactoryDeclaration<MyLibComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MyLibComponent, "lib-my-lib", never, {}, {}, never, never>;
-}
