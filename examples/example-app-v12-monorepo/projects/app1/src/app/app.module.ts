8799bb8365fc1d7f149ede540cf6a47a87812e4b
rfprod <zoidenmacht@zoho.com>
2021-11-18 04:12:39 +0300

    test(unit): angular v12 monorepo example app unit tests
    - [ ] add angular v12 monorepo example app unit tests;

--- examples/example-app-v12-monorepo/projects/app1/src/app/app.module.ts
+++ examples/example-app-v12-monorepo/projects/app1/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
+import { APP_ENVIRONMENT } from './configs/environment.config';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_ENVIRONMENT,
+      useValue: environment,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
