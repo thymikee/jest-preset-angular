9f6838cef9274f80bbe3d75c616795b255411d5a
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2021-11-23 19:58:20 +0100

    build(devs-dep): update `jest-preset-angular` for example apps

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,44 +1,48 @@
 const chalk = require('chalk');
 const execa = require('execa');
-const path = require('path');
 
 const buildDir = 'build';
-const ngTransformerPath = `${process.cwd()}/src/transformers/downlevel_decorators_transform`;
+const ngTransformerPath = `./src/transformers/downlevel_decorators_transform`;
 const bazelFileGlob = 'src/**/BUILD.bazel';
 const ngTestFolder = 'src/ngtsc/reflection/test';
 const ngTransformerURL =
   'https://github.com/angular/angular/tree/master/packages/compiler-cli/src/transformers/downlevel_decorators_transform';
 const ngReflectionURL = 'https://github.com/angular/angular/tree/master/packages/compiler-cli/src/ngtsc/reflection';
-const transformerFolder = path.join('src', 'transformers');
-const reflectionFolder = path.join('src', 'ngtsc', path.sep);
+const transformersFolder = './src/transformers';
+const ngtscFolder = './src/ngtsc';
 
 process.stdout.write(chalk.green('  \u2022 ') + 'clean up ' + chalk.blue(buildDir) + '\n');
 
 execa.sync('yarn', ['rimraf', buildDir]);
 
-process.stdout.write(chalk.green('  \u2022 ') + 'clean up ' + chalk.blue(ngTransformerPath) + '\n');
+process.stdout.write(
+  chalk.green('  \u2022 ') +
+    'clean up previously downloaded assets' +
+    chalk.blue(`${ngTransformerPath}, ${ngtscFolder}`) +
+    '\n'
+);
 
-execa.sync('yarn', ['rimraf', `${process.cwd()}/src/transformers/downlevel_decorators_transform`]);
+execa.sync('yarn', ['rimraf', ngTransformerPath, ngtscFolder]);
 
 process.stdout.write(
   chalk.green('  \u2022 ') +
     `downloading Angular Downlevel Decorator Transformer from ${ngTransformerURL}` +
     chalk.green(' \u21D2 ') +
-    transformerFolder +
+    transformersFolder +
     '\n'
 );
 
-execa.sync('yarn', ['fetcher', `--url=${ngTransformerURL}`, `--out=${transformerFolder}`]);
+execa.sync('yarn', ['fetcher', `--url=${ngTransformerURL}`, `--out=${transformersFolder}`]);
 
 process.stdout.write(
   chalk.green('  \u2022 ') +
     `downloading dependencies for Angular Downlevel Decorator Transformer from ${ngReflectionURL}` +
     chalk.green(' \u21D2 ') +
-    reflectionFolder +
+    ngtscFolder +
     '\n'
 );
 
-execa.sync('yarn', ['fetcher', `--url=${ngReflectionURL}`, `--out=${reflectionFolder}`]);
+execa.sync('yarn', ['fetcher', `--url=${ngReflectionURL}`, `--out=${ngtscFolder}`]);
 
 process.stdout.write(chalk.green('  \u2022 ') + 'clean up Bazel files ' + chalk.blue(bazelFileGlob) + '\n');
 
