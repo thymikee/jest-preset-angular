386a6be6109417e5e38defd0a347345fc32734ad
Vadim <zraw.stdio@gmail.com>
2021-12-03 20:34:12 +0300

    test(examples): angular v11 example app and unit tests (the tour of heroes) (#1182)

--- examples/example-app-v11/src/app/app-routing.module.ts
+++ examples/example-app-v11/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', loadChildren: () => import('./hero/hero.module').then((m) => m.HeroModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
