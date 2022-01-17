445f25aa420898f997d05ce98d62d603e7e2047a
John Crim <johncrim@users.noreply.github.com>
2022-01-16 19:54:14 -0800

    improve(example v13): Add `destroyAfterEach: true` to example

--- examples/example-app-v13/setup-jest.ts
+++ examples/example-app-v13/setup-jest.ts
@@ -7,4 +7,8 @@ import './jest-global-mocks';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: {
+    destroyAfterEach: true,
+  },
+});
