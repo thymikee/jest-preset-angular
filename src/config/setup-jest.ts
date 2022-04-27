084bf9cc2100343d7b3f03df4fca896a0fcc4e55
anh.pham <anh.pham@cm.com>
2022-04-27 11:09:56 +0200

    feat: remove `zone-patch` and its fallback
    BREAKING CHANGE
    Since Angular 11, the minimum version of `zone.js` is 0.11.x and Angular 10 is EOL so now we can use zone patch directly from `zone.js`. If one is not using `zone.js@0.11.x`, please upgrade.

--- src/config/setup-jest.ts
+++ src/config/setup-jest.ts
@@ -1,26 +1,11 @@
 require('../utils/reflect-metadata');
 
-try {
-  // If the user are using zone.js 0.11.1+
-  // all jest support logic are implemented inside zone.js
-  // we only need to load zone-testing module
-  !process.execArgv.includes('--experimental-vm-modules')
-    ? require('zone.js/bundles/zone-testing-bundle.umd.js')
-    : require('zone.js/fesm2015/zone-testing-bundle.min.js');
-} catch (err) {
-  // Fallback logic to load zone and zone-patch
-  // when the user still use zone.js 0.10.x
-  require('zone.js/dist/zone');
-  require('zone.js/dist/proxy');
-  require('zone.js/dist/sync-test');
-  require('zone.js/dist/async-test');
-  require('zone.js/dist/fake-async-test');
-  require('../zone-patch');
-}
+!process.execArgv.includes('--experimental-vm-modules')
+  ? require('zone.js/bundles/zone-testing-bundle.umd.js')
+  : require('zone.js/fesm2015/zone-testing-bundle.min.js');
 
 const getTestBed = require('@angular/core/testing').getTestBed;
-const BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
-const platformBrowserDynamicTesting = require('@angular/platform-browser-dynamic/testing')
-  .platformBrowserDynamicTesting;
+const { BrowserDynamicTestingModule } = require('@angular/platform-browser-dynamic/testing');
+const { platformBrowserDynamicTesting } = require('@angular/platform-browser-dynamic/testing')
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
