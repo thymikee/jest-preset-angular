55f4375c6671319c5ab5579c03f3fa06e8ade320
Ahn <27772165+ahnpnl@users.noreply.github.com>
2022-04-27 12:48:26 +0200

    feat: use `import 'zone.js'` and `import 'zone.js/testing'` in `setup-jest` (#1429)
    BREAKING CHANGE
    `setup-jest` now is identical to the file which is generated by Angular CLI for Karma + Jasmine, `test.ts`, in which `jest-preset-angular` put
    ```
    import 'zone.js';
    import 'zone.js/testing';
    ```

--- src/config/setup-jest.ts
+++ src/config/setup-jest.ts
@@ -1,9 +1,6 @@
-!process.execArgv.includes('--experimental-vm-modules')
-  ? require('zone.js/bundles/zone-testing-bundle.umd.js')
-  : require('zone.js/fesm2015/zone-testing-bundle.min.js');
-
-const getTestBed = require('@angular/core/testing').getTestBed;
-const { BrowserDynamicTestingModule } = require('@angular/platform-browser-dynamic/testing');
-const { platformBrowserDynamicTesting } = require('@angular/platform-browser-dynamic/testing')
+import 'zone.js';
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
