4f3f6ceae0f19fd4f28a75147353a52d27afa31f
anh.pham <anh.pham@cm.com>
2022-04-27 12:28:36 +0200

    feat: use `import 'zone.js'` and `import 'zone.js/testing'`
    BREAKING CHANGE
    `setup-jest` now is identical to the file which is generated by Angular CLI for Karma + Jasmine, `test.ts`, in which `jest-preset-angular` put
    ```
    import 'zone.js';
    import 'zone.js/testing'
    ```

--- src/config/setup-jest.spec.ts
+++ src/config/setup-jest.spec.ts
@@ -1,7 +1,23 @@
 const mockInitTestEnvironment = jest.fn();
+const mockZoneJs = jest.fn();
+const mockZoneJsTesting = jest.fn();
 const mockGetTestBed = jest.fn(() => {
   return {
     initTestEnvironment: mockInitTestEnvironment,
+  };
+});
+jest.mock('zone.js', () => {
+  const mockedResult = mockZoneJs();
+
+  return {
+    mockedResult,
+  };
+});
+jest.mock('zone.js/testing', () => {
+  const mockedResult = mockZoneJsTesting();
+
+  return {
+    mockedResult,
   };
 });
 jest.mock('@angular/core/testing', () => {
@@ -21,9 +37,10 @@ jest.mock('@angular/platform-browser-dynamic/testing', () => {
 });
 
 test('should initialize test environment with getTestBed() and initTestEnvironment()', async () => {
-  // @ts-expect-error testing purpose
   await import('../config/setup-jest');
 
+  expect(mockZoneJs).toHaveBeenCalled();
+  expect(mockZoneJsTesting).toHaveBeenCalled();
   expect(mockGetTestBed).toHaveBeenCalled();
   expect(mockInitTestEnvironment).toHaveBeenCalled();
   expect(mockInitTestEnvironment.mock.calls[0][0]).toBeInstanceOf(BrowserDynamicTestingModuleStub);
