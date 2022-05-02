06a1cac53f28b9b517f56b0b5af7a8e3c6388129
anh.pham <anh.pham@cm.com>
2022-05-02 23:13:13 +0200

    feat(presets): use `.mjs` extension for ESM preset
    BREAKING CHANGE
    ESM preset now uses `.mjs` extensions which cannot use with `require` syntax if one is doing it.

---
+++ presets/defaults-esm/jest-preset.d.ts
@@ -0,0 +1,34 @@
+declare const _default: {
+  extensionsToTreatAsEsm: string[];
+  globals: {
+    'ts-jest': {
+      useESM: boolean;
+      tsconfig?: string | boolean | import('ts-jest').RawCompilerOptions | undefined;
+      isolatedModules?: boolean | undefined;
+      compiler?: string | undefined;
+      astTransformers?: import('ts-jest').ConfigCustomTransformer | undefined;
+      diagnostics?:
+        | boolean
+        | {
+            pretty?: boolean | undefined;
+            ignoreCodes?: string | number | Array<string | number> | undefined;
+            exclude?: string[] | undefined;
+            warnOnly?: boolean | undefined;
+          }
+        | undefined;
+      babelConfig?: string | boolean | import('@babel/core').TransformOptions | undefined;
+      stringifyContentPathRegex?: string | RegExp | undefined;
+    };
+  };
+  moduleNameMapper: {
+    tslib: string;
+  };
+  transform: {
+    '^.+\\.(ts|js|html|svg)$': string;
+  };
+  transformIgnorePatterns: string[];
+  testEnvironment: string;
+  moduleFileExtensions: string[];
+  snapshotSerializers: string[];
+};
+export default _default;
--- src/presets/index.spec.ts
+++ src/presets/index.spec.ts
@@ -1,14 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 
-import presets from '../../presets';
+import { defaultPreset, defaultEsmPreset } from './';
 
 describe('Jest presets', () => {
-  test.each([presets.defaults, presets.defaultsESM])('should return the correct jest config', (preset) => {
+  test.each([defaultPreset, defaultEsmPreset])('should return the correct jest config', (preset) => {
     expect(preset).toMatchSnapshot();
   });
 
-  test('should have the correct types which come from `ts-jest`', () => {
-    expect(fs.readFileSync(path.join(__dirname, '..', '..', 'presets', 'index.d.ts'), 'utf-8')).toMatchSnapshot();
+  test.each([
+    path.join(__dirname, '..', '..', 'presets/defaults/jest-preset.d.ts'),
+    path.join(__dirname, '..', '..', 'presets/defaults-esm/jest-preset.d.ts'),
+  ])('should have the correct types which come from `ts-jest`', (presetTypes) => {
+    expect(fs.readFileSync(presetTypes, 'utf-8')).toMatchSnapshot();
   });
 });
