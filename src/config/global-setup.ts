038bc708582546b13d8b6c7b517c448d64acf139
Rafael Siqueira <rafael.siqueira@un.org>
2022-06-25 16:09:09 -0400

    Allows default and named export for global-setup

--- src/config/global-setup.ts
+++ src/config/global-setup.ts
@@ -1,9 +1,11 @@
 import { runNgccJestProcessor } from '../utils/ngcc-jest-processor';
 
-export = async () => {
+const GlobalSetup = () => {
   const ngJestConfig = globalThis.ngJest;
   const tsconfig = ngJestConfig?.tsconfig;
   if (!ngJestConfig?.skipNgcc) {
     runNgccJestProcessor(tsconfig);
   }
-};
+}
+
+export default GlobalSetup;
