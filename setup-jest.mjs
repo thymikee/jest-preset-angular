6886e653a1c9798358f19535317de03b837b51ef
anh.pham <anh.pham@cm.com>
2022-09-11 16:40:16 +0200

    feat: remove `destroyAfterEach` and `teardown` options
    BREAKING CHANGE
    `destroyAfterEach` and `teardown` are no longer available to use, please use `testEnvironmentOptions` instead.

--- setup-jest.mjs
+++ setup-jest.mjs
@@ -2,36 +2,6 @@ import 'zone.js/fesm2015/zone-testing-bundle.min.js';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-let testEnvironmentOptions = globalThis.ngJest?.testEnvironmentOptions ?? Object.create(null);
-
-const configuredDestroyAfterEach = globalThis.ngJest?.destroyAfterEach;
-if (configuredDestroyAfterEach) {
-  console.warn(
-    'Passing destroyAfterEach for configuring the test environment has been deprecated.' +
-      ' Please pass a `testEnvironmentOptions` object with TestEnvironmentOptions interface instead,' +
-      ' see https://angular.io/api/core/testing/TestEnvironmentOptions',
-  );
-
-  testEnvironmentOptions = {
-    ...testEnvironmentOptions,
-    teardown: {
-      destroyAfterEach: true,
-    },
-  };
-}
-
-const configuredTeardown = globalThis.ngJest?.teardown;
-if (configuredTeardown) {
-  console.warn(
-    'Passing teardown for configuring the test environment has been deprecated.' +
-      ' Please pass a `testEnvironmentOptions` object with TestEnvironmentOptions interface instead,' +
-      ' see https://angular.io/api/core/testing/TestEnvironmentOptions',
-  );
-
-  testEnvironmentOptions = {
-    ...testEnvironmentOptions,
-    teardown: configuredTeardown,
-  };
-}
+const testEnvironmentOptions = globalThis.ngJest?.testEnvironmentOptions ?? Object.create(null);
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), testEnvironmentOptions);
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,14 +1,12 @@
 /* eslint-disable */
 
-import type { ModuleTeardownOptions, TestEnvironmentOptions } from '@angular/core/testing';
+import type { TestEnvironmentOptions } from '@angular/core/testing';
 
 declare global {
   var ngJest:
     | {
         skipNgcc?: boolean;
         tsconfig?: string;
-        destroyAfterEach?: boolean;
-        teardown?: ModuleTeardownOptions;
         testEnvironmentOptions?: TestEnvironmentOptions;
       }
     | undefined;
