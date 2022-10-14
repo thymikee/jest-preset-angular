4c48963101e1ad246078dbe00a2f9bab14b05128
RA-arnost-dudek <115218822+RA-arnost-dudek@users.noreply.github.com>
2022-10-14 16:08:10 +0200

    fix: typo in esbuild-check.js

--- esbuild-check.js
+++ esbuild-check.js
@@ -1,7 +1,7 @@
 // If the platform does not support the native variant of esbuild, this will crash.
 // This script can then be spawned by the CLI to determine if native usage is supported.
 require('esbuild')
-  .formatMessages([], { kind: 'error ' })
+  .formatMessages([], { kind: 'error' })
   .then(
     () => {},
     () => {}
