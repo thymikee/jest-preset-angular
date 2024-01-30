a82925cc11dd577bab5ded2167dc46bb4a31a1c7
Anh Pham <anh.pham@cm.com>
2024-01-30 06:36:03 +0100

    feat: support signal input in 17.1

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,55 +1,10 @@
-const chalk = require('chalk');
-const execa = require('execa');
+const { buildSync } = require('esbuild');
 
-const buildDir = 'build';
-const ngTransformerPath = `./src/transformers/downlevel_decorators_transform`;
-const bazelFileGlob = 'src/**/BUILD.bazel';
-const ngTestFolder = 'src/ngtsc/reflection/test';
-const ngTransformerURL =
-  'https://github.com/angular/angular/blob/15.0.4/packages/compiler-cli/src/transformers/downlevel_decorators_transform';
-const ngReflectionURL = 'https://github.com/angular/angular/blob/15.0.4/packages/compiler-cli/src/ngtsc/reflection';
-const tsCompatUrl = 'https://github.com/angular/angular/blob/15.0.4/packages/compiler-cli/src/ngtsc/ts_compatibility';
-const transformersFolder = './src/transformers';
-const ngtscFolder = './src/ngtsc';
-
-process.stdout.write(chalk.green('  \u2022 ') + 'clean up ' + chalk.blue(buildDir) + '\n');
-
-execa.sync('yarn', ['rimraf', buildDir]);
-
-process.stdout.write(
-  chalk.green('  \u2022 ') +
-    'clean up previously downloaded assets' +
-    chalk.blue(`${ngTransformerPath}, ${ngtscFolder}`) +
-    '\n'
-);
-
-execa.sync('yarn', ['rimraf', ngTransformerPath, ngtscFolder]);
-
-process.stdout.write(
-  chalk.green('  \u2022 ') +
-    `downloading Angular Downlevel Decorator Transformer from ${ngTransformerURL}` +
-    chalk.green(' \u21D2 ') +
-    transformersFolder +
-    '\n'
-);
-
-execa.sync('yarn', ['fetcher', `--url=${ngTransformerURL}`, `--out=${transformersFolder}`]);
-
-process.stdout.write(
-  chalk.green('  \u2022 ') +
-    `downloading dependencies for Angular Downlevel Decorator Transformer from ${[ngReflectionURL, tsCompatUrl]}` +
-    chalk.green(' \u21D2 ') +
-    ngtscFolder +
-    '\n'
-);
-
-execa.sync('yarn', ['fetcher', `--url=${ngReflectionURL}`, `--out=${ngtscFolder}`]);
-execa.sync('yarn', ['fetcher', `--url=${tsCompatUrl}`, `--out=${ngtscFolder}`]);
-
-process.stdout.write(chalk.green('  \u2022 ') + 'clean up Bazel files ' + chalk.blue(bazelFileGlob) + '\n');
-
-execa.sync('yarn', ['rimraf', bazelFileGlob]);
-
-process.stdout.write(chalk.green('  \u2022 ') + 'clean up Angular test files ' + chalk.blue(ngTestFolder) + '\n');
-
-execa.sync('yarn', ['rimraf', ngTestFolder]);
+buildSync({
+  entryPoints: ['src/transformers/downlevel-ctor.ts'],
+  outdir: 'build',
+  external: ['esbuild', 'ts-jest', 'typescript'],
+  platform: 'node',
+  target: 'node16',
+  bundle: true,
+});
--- src/transformers/downlevel-ctor.ts
+++ src/transformers/downlevel-ctor.ts
@@ -1,3 +1,4 @@
+import { getInputSignalsMetadataTransform } from '@angular/compiler-cli/src/transformers/jit_transforms';
 import type ts from 'typescript';
 
 import { TypeScriptReflectionHost } from '../ngtsc/reflection';
@@ -7,6 +8,18 @@ import { getDownlevelDecoratorsTransform } from './downlevel_decorators_transfor
 export const constructorParametersDownlevelTransform = (program: ts.Program): ts.TransformerFactory<ts.SourceFile> => {
   const typeChecker = program.getTypeChecker();
   const reflectionHost = new TypeScriptReflectionHost(typeChecker);
+  const inputSignalMetadataTransform = getInputSignalsMetadataTransform(reflectionHost, false);
 
-  return getDownlevelDecoratorsTransform(typeChecker, reflectionHost, [], false, false, true);
+  return (ctx) => {
+    return (sourceFile) => {
+      if (inputSignalMetadataTransform) {
+        sourceFile = inputSignalMetadataTransform(ctx)(sourceFile);
+      }
+      sourceFile = getDownlevelDecoratorsTransform(typeChecker, reflectionHost, [], false, false, true)(ctx)(
+        sourceFile,
+      );
+
+      return sourceFile;
+    };
+  };
 };
