c8b84a8f35a6d8fb89ff751fe24a34e819f45810
Anh Pham <anh.pham@cm.com>
2024-02-16 09:24:21 +0100

    feat: support signal queries in Angular 17.2

---
+++ e2e/ast-transformers/ng-jit-transformers/__tests__/signal-queries.spec.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+test('signal queries', () => {
+  @Component({
+    selector: 'greet',
+    standalone: true,
+    template: '<input #el>',
+  })
+  class GreetCmp {
+    input = viewChild.required<ElementRef>('el');
+  }
+
+  const fixture = TestBed.createComponent(GreetCmp);
+  fixture.detectChanges();
+
+  expect(fixture.componentInstance.input()).toBeDefined();
+});
