7b8c43c36ea5931a86baa9889f81da2457a11c4d
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2024-05-14 10:07:16 +0700

    test(e2e): update tsconfig for `example-app-monorepo`

--- examples/example-app-monorepo/apps/app1/jest.config.js
+++ examples/example-app-monorepo/apps/app1/jest.config.js
@@ -1,9 +1,10 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { paths } = require('./tsconfig.json').compilerOptions;
+
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   displayName: 'app1',
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  moduleNameMapper: {
-    'libs/(.*)$': '<rootDir>/../../libs/$1',
-  },
+  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
 };
