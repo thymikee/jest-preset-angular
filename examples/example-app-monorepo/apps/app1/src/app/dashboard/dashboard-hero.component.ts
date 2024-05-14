816112cae9194261a6069da361aabd6fc6bc2207
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2024-05-14 12:04:07 +0700

    test(e2e): update example app monorepo with signals

--- examples/example-app-monorepo/apps/app1/src/app/dashboard/dashboard-hero.component.ts
+++ examples/example-app-monorepo/apps/app1/src/app/dashboard/dashboard-hero.component.ts
@@ -1,5 +1,5 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 import { Hero } from '../model';
 
@@ -8,16 +8,17 @@ import { Hero } from '../model';
   selector: 'dashboard-hero',
   template: `
     <button type="button" (click)="click()" class="hero">
-      {{ hero.name | uppercase }}
+      {{ hero().name | uppercase }}
     </button>
   `,
   styleUrls: ['./dashboard-hero.component.css'],
   imports: [UpperCasePipe],
 })
 export class DashboardHeroComponent {
-  @Input() hero!: Hero;
-  @Output() selected = new EventEmitter<Hero>();
+  hero = input.required<Hero>();
+  readonly selected = output<Hero>();
+
   click() {
-    this.selected.emit(this.hero);
+    this.selected.emit(this.hero());
   }
 }
