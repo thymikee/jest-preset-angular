01e4957740609b7c454c9ec562d8bba0306df36e
ahnpnl <anhpnnd@gmail.com>
2024-07-22 00:49:57 +0200

    refactor(serializers): add option to remove comp attributes
    Closes #2578
    Closes #2580

--- src/serializers/ng-snapshot.ts
+++ src/serializers/ng-snapshot.ts
@@ -4,15 +4,35 @@ import type { OldPlugin } from 'pretty-format';
 
 const attributesToRemovePatterns = ['__ngContext__'];
 
-const print: OldPlugin['print'] = (fixture, printer, indent, opts, colors) => {
-    let componentAttrs = '';
+type PluginPrintFnArgs = Parameters<OldPlugin['print']>;
+
+type NgSnapshotOptions = {
+    omitAllCompAttrs?: boolean;
+};
+
+type PluginPrintFn = (
+    fixture: PluginPrintFnArgs[0],
+    printer: PluginPrintFnArgs[1],
+    indent: PluginPrintFnArgs[2],
+    opts: PluginPrintFnArgs[3] & NgSnapshotOptions,
+    colors: PluginPrintFnArgs[4],
+) => string;
+
+const removeTrailingWhiteSpaces = (serializedComponent: string): string => {
+    return serializedComponent.replace(/\n^\s*\n/gm, '\n');
+};
+
+const print: PluginPrintFn = (fixture, printer, indent, opts, colors) => {
     const { componentRef, componentInstance } = fixture as ComponentFixture<Record<string, unknown>>;
     const componentDef = (componentRef.componentType as ɵComponentType<unknown>).ɵcmp as ɵDirectiveDef<unknown>;
     const componentName = componentDef.selectors[0][0] as string;
     const nodes = Array.from(componentRef.location.nativeElement.childNodes).map(printer).join('');
-    const attributes = Object.keys(componentInstance).filter((key) => !attributesToRemovePatterns.includes(key));
-    if (attributes.length) {
-        componentAttrs += attributes
+    let serializedComponent = '';
+    if (opts.omitAllCompAttrs) {
+        serializedComponent = '<' + componentName + '>\n' + indent(nodes) + '\n</' + componentName + '>';
+    } else {
+        const attributes = Object.keys(componentInstance).filter((key) => !attributesToRemovePatterns.includes(key));
+        const componentAttrs = attributes
             .sort()
             .map((attribute) => {
                 const compAttrVal = componentInstance[attribute];
@@ -28,19 +48,21 @@ const print: OldPlugin['print'] = (fixture, printer, indent, opts, colors) => {
                 );
             })
             .join('');
+        serializedComponent =
+            '<' +
+            componentName +
+            componentAttrs +
+            (componentAttrs.length ? '\n' : '') +
+            '>\n' +
+            indent(nodes) +
+            '\n</' +
+            componentName +
+            '>';
     }
 
-    return (
-        '<' +
-        componentName +
-        componentAttrs +
-        (componentAttrs.length ? '\n' : '') +
-        '>\n' +
-        indent(nodes) +
-        '\n</' +
-        componentName +
-        '>'
-    ).replace(/\n^\s*\n/gm, '\n');
+    serializedComponent = removeTrailingWhiteSpaces(serializedComponent);
+
+    return serializedComponent;
 };
 
 const test: OldPlugin['test'] = (val) => !!val && typeof val === 'object' && 'componentRef' in val;
