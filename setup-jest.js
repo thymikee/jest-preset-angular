a3040363e58cc0f44bddf702db6afa23a309a818
ahnpnl <anhpnnd@gmail.com>
2024-11-15 16:47:22 +0700

    feat: deprecate `setup-jest.js` and `setup-jest.mjs`
    DEPRECATION
    
    Importing `setup-jest.js`/`setup-jest.mjs` directly is deprecated. The files will be removed in the future.
    
    Please use "setupZoneTestEnv" function instead. Example:
    
    For CJS
    ```
    // setup-jest.ts
    import { setupZoneTestEnv } from 'jest-preset-angular/setup-env';
    
    setupZoneTestEnv();
    ```
    
    For ESM
    ```
    // setup-jest.ts
    import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/index.mjs';
    
    setupZoneTestEnv();
    ```

--- setup-jest.js
+++ setup-jest.js
@@ -1,3 +1,13 @@
+console.warn(`
+    Importing "setup-jest.js" directly is deprecated. The file "setup-jest.js" will be removed in the future.
+    Please use "setupZoneTestEnv" function instead. Example:
+
+    // setup-jest.ts
+    import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+    setupZoneTestEnv();
+`);
+
 require('zone.js');
 require('zone.js/testing');
 const { TextEncoder, TextDecoder } = require('util');
--- setup-jest.mjs
+++ setup-jest.mjs
@@ -1,8 +1,19 @@
+console.warn(`
+    Importing "setup-jest.mjs" directly is deprecated. The file "setup-jest.mjs" will be removed in the future.
+    Please use "setupZoneTestEnv" function instead. Example:
+
+    // setup-jest.ts
+    import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone/index.mjs';
+
+    setupZoneTestEnv();
+`);
+
 import 'zone.js';
 import 'zone.js/testing';
+import { TextEncoder, TextDecoder } from 'util';
+
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { TextEncoder, TextDecoder } from 'util';
 
 if (typeof globalThis.TextEncoder === 'undefined') {
     globalThis.TextEncoder = TextEncoder;
