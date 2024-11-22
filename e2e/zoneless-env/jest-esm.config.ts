fe2fbe9da2d6160b8df9496d8fe6b9c810ac449c
ahnpnl <anhpnnd@gmail.com>
2024-11-22 09:57:39 +0700

    feat: support configuring zoneless test env for CJS

---
+++ e2e/zoneless-env/jest-esm.config.ts
@@ -0,0 +1,28 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+    displayName: 'e2e-zoneless-env',
+    testEnvironment: 'jsdom',
+    snapshotSerializers: [
+        '<rootDir>/../../build/serializers/html-comment',
+        '<rootDir>/../../build/serializers/ng-snapshot',
+        '<rootDir>/../../build/serializers/no-ng-attributes',
+    ],
+    setupFilesAfterEnv: ['./setup-zoneless-env.mts'],
+    moduleNameMapper: {
+        rxjs: '<rootDir>/../../node_modules/rxjs/dist/bundles/rxjs.umd.js',
+    },
+    extensionsToTreatAsEsm: ['.ts', '.mts'],
+    transform: {
+        '^.+\\.(ts|mts|js|mjs|html)$': [
+            '<rootDir>/../../build/index.js',
+            {
+                useESM: true,
+                tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+                stringifyContentPathRegex: '\\.(html|svg)$',
+            },
+        ],
+    },
+};
+
+export default config;
--- setup-env/zone/index.js
+++ setup-env/zone/index.js
@@ -1,38 +1,20 @@
 require('zone.js');
 require('zone.js/testing');
 
-const { TextEncoder, TextDecoder } = require('util');
-
 const { getTestBed } = require('@angular/core/testing');
 const {
     BrowserDynamicTestingModule,
     platformBrowserDynamicTesting,
 } = require('@angular/platform-browser-dynamic/testing');
 
-const polyfillEncoder = () => {
-    if (typeof globalThis.TextEncoder === 'undefined') {
-        globalThis.TextEncoder = TextEncoder;
-        globalThis.TextDecoder = TextDecoder;
-    }
-};
-
-const resolveTestEnvOptions = (options) => {
-    const globalTestEnvOptions = globalThis.ngJest?.testEnvironmentOptions;
-    if (globalTestEnvOptions) {
-        console.warn(
-            'Setting testEnvironmentOptions via globalThis.ngJest is deprecated. Please provide testEnvironmentOptions via function argument',
-        );
-    }
-
-    return globalTestEnvOptions ?? options;
-};
+const { polyfillEncoder, resolveTestEnvOptions } = require('../utils');
 
 const setupZoneTestEnv = (options) => {
     polyfillEncoder();
     const testEnvironmentOptions = resolveTestEnvOptions(options);
 
     getTestBed().initTestEnvironment(
-        BrowserDynamicTestingModule,
+        [BrowserDynamicTestingModule],
         platformBrowserDynamicTesting(),
         testEnvironmentOptions,
     );
