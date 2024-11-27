8da56306bd61b31e556b5cf48b0012f8f73f15a0
ahnpnl <anhpnnd@gmail.com>
2024-11-27 17:31:32 +0700

    refactor: deprecate legacy presets
    DEPRECATION
    
    Using `preset: 'jest-preset-angular'` is deprecated. The recommended approach is https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createcjspresetoptions
    
    Using `preset: 'jest-preset-angular/presets/defaults-esm'` is deprecated. The recommended approach is https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createesmpresetoptions

--- presets/index.js
+++ presets/index.js
@@ -1,8 +1,22 @@
 const ngJestPresets = require('../build/presets');
 
 module.exports = {
-    defaults: ngJestPresets.defaultPreset,
-    defaultsESM: ngJestPresets.defaultEsmPreset,
+    get defaults() {
+        console.warn(`
+            This preset is DEPRECATED and will be removed in the future.
+            Please use "createCjsPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createcjspresetoptions
+        `);
+
+        return ngJestPresets.defaultPreset;
+    },
+    get defaultsESM() {
+        console.warn(`
+            This preset is DEPRECATED and will be removed in the future.
+            Please use "createCjsPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createesmpresetoptions
+        `);
+
+        return ngJestPresets.defaultEsmPreset;
+    },
     defaultTransformerOptions: ngJestPresets.defaultTransformerOptions,
     createCjsPreset: ngJestPresets.createCjsPreset,
     createEsmPreset: ngJestPresets.createEsmPreset,
