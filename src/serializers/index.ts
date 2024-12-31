dd64d78c009bf88bf44d97931ad39f692a43d190
ahnpnl <anhpnnd@gmail.com>
2024-12-31 10:35:30 +0100

    refactor: add type for `serializers/index.ts`

--- src/serializers/index.ts
+++ src/serializers/index.ts
@@ -1,5 +1,7 @@
+import type { Config } from 'jest';
+
 export = [
     'jest-preset-angular/build/serializers/html-comment',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/no-ng-attributes',
-];
+] satisfies Config['snapshotSerializers'];
