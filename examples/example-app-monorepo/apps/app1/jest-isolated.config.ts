99d3112062ea0f95b6f40768560052b983327df2
ahnpnl <anhpnnd@gmail.com>
2024-12-31 09:50:34 +0100

    refactor: deprecate `defaultTransformerOptions` in `presets`

--- examples/example-app-monorepo/apps/app1/jest-isolated.config.ts
+++ examples/example-app-monorepo/apps/app1/jest-isolated.config.ts
@@ -1,17 +1,11 @@
+import type { Config } from 'jest';
 import presets from 'jest-preset-angular/presets';
-import type { JestConfigWithTsJest } from 'ts-jest';
 
 import jestCfg from './jest.config';
 
 export default {
     ...jestCfg,
-    transform: {
-        '^.+\\.(ts|js|mjs|html|svg)$': [
-            'jest-preset-angular',
-            {
-                ...presets.defaultTransformerOptions,
-                isolatedModules: true,
-            },
-        ],
-    },
-} satisfies JestConfigWithTsJest;
+    ...presets.createCjsPreset({
+        isolatedModules: true,
+    }),
+} satisfies Config;
--- presets/index.js
+++ presets/index.js
@@ -3,7 +3,7 @@ const ngJestPresets = require('../build/presets');
 module.exports = {
     get defaults() {
         console.warn(`
-            This preset is DEPRECATED and will be removed in the future.
+            This preset is DEPRECATED and will be removed in the next major release.
             Please use "createCjsPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createcjspresetoptions
         `);
 
@@ -11,13 +11,20 @@ module.exports = {
     },
     get defaultsESM() {
         console.warn(`
-            This preset is DEPRECATED and will be removed in the future.
-            Please use "createCjsPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createesmpresetoptions
+            This preset is DEPRECATED and will be removed in the next major release.
+            Please use "createEsmPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets#createesmpresetoptions
         `);
 
         return ngJestPresets.defaultEsmPreset;
     },
-    defaultTransformerOptions: ngJestPresets.defaultTransformerOptions,
+    get defaultTransformerOptions() {
+        console.warn(`
+            These options are DEPRECATED and will be removed in the next major release.
+            Please use "createCjsPreset" or "createEsmPreset" function instead. See documentation at https://thymikee.github.io/jest-preset-angular/docs/getting-started/presets
+        `);
+
+        return ngJestPresets.defaultTransformerOptions;
+    },
     createCjsPreset: ngJestPresets.createCjsPreset,
     createEsmPreset: ngJestPresets.createEsmPreset,
 };
