947fd4f31b4a14bbe7d575f9a363f2f10566b258
ahnpnl <anhpnnd@gmail.com>
2025-02-28 10:33:15 +0100

    refactor: remove `defaults` and `defaults-esm` presets
    BREAKING CHANGE
    
    * Use `createCjsPreset` as a replacement for `defaults` preset
    * Use `createEsmPreset` as a replacement for `defaults-esm` preset

--- presets/index.d.ts
+++ presets/index.d.ts
@@ -1,23 +1,4 @@
 declare const _default: {
-    defaults: {
-        transformIgnorePatterns: string[];
-        transform: import('ts-jest').JestConfigWithTsJest['transform'];
-        testEnvironment: string;
-        moduleFileExtensions: string[];
-        snapshotSerializers: string[];
-    };
-    defaultsESM: {
-        extensionsToTreatAsEsm: string[];
-        moduleNameMapper: {
-            tslib: string;
-        };
-        transform: import('ts-jest').JestConfigWithTsJest['transform'];
-        transformIgnorePatterns: string[];
-        testEnvironment: string;
-        moduleFileExtensions: string[];
-        snapshotSerializers: string[];
-    };
-    defaultTransformerOptions: import('ts-jest').TsJestTransformerOptions;
     createCjsPreset: typeof import('../build/presets').createCjsPreset;
     createEsmPreset: typeof import('../build/presets').createEsmPreset;
 };
