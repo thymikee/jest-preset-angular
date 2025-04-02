7c197a54c5a562779677128324baa7244dddaea9
ahnpnl <anhpnnd@gmail.com>
2025-04-02 22:49:42 +0200

    test: improve perf test

--- performance/jest.config.ts
+++ performance/jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    testEnvironment: 'jsdom',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': '<rootDir>/../build/index.js',
     },
