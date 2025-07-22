f024f2124a19e5620f7b4d468e5b90bdccd35be8
ahnpnl <anhpnnd@gmail.com>
2025-07-22 13:55:11 +0200

    test: add demo custom test jsdom env in `example-app-v18`

--- examples/example-app-v18/jest-esm.config.ts
+++ examples/example-app-v18/jest-esm.config.ts
@@ -4,7 +4,10 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 
 import tsconfig from './tsconfig.json';
 
-const esmPreset = createEsmPreset();
+const esmPreset = createEsmPreset({
+    tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+    testEnvironment: 'jest-preset-angular/environments/jest-jsdom-env',
+});
 
 export default {
     ...esmPreset,
@@ -14,14 +17,4 @@ export default {
         '^rxjs': '<rootDir>/node_modules/rxjs/dist/bundles/rxjs.umd.js',
     },
     setupFilesAfterEnv: ['<rootDir>/setup-jest-esm.ts'],
-    transform: {
-        '^.+\\.(ts|js|html|svg)$': [
-            'jest-preset-angular',
-            {
-                tsconfig: '<rootDir>/tsconfig-esm.spec.json',
-                stringifyContentPathRegex: '\\.(html|svg)$',
-                useESM: true,
-            },
-        ],
-    },
 } satisfies Config;
