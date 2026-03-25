f2c7c2f48cb5f1b6abbaeee689cf7cdee1ca2ad0
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2026-03-25 20:49:46 +0100

    fix: prettier

--- e2e/snapshot-serializers/material-cdk-test.component.ts
+++ e2e/snapshot-serializers/material-cdk-test.component.ts
@@ -4,19 +4,11 @@ import { Component } from '@angular/core';
     selector: 'material-cdk-test',
     standalone: true,
     template: `
-        <div
-            id="cdk-step-content-0-0"
-            aria-labelledby="cdk-step-label-0-0"
-            aria-controls="cdk-step-content-2-0"
-        >
+        <div id="cdk-step-content-0-0" aria-labelledby="cdk-step-label-0-0" aria-controls="cdk-step-content-2-0">
             Step content
         </div>
         <label for="mat-form-field-label-9">Field label</label>
-        <input
-            id="mat-input-4"
-            name="mat-button-toggle-group-a0"
-            aria-owns="mat-input-4"
-        />
+        <input id="mat-input-4" name="mat-button-toggle-group-a0" aria-owns="mat-input-4" />
         <span class="ng-tns-c25-1 ng-star-inserted custom-class">Styled text</span>
     `,
 })
